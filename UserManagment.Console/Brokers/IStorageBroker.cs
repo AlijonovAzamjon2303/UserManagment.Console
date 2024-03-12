ec2ded932d1fa8b31d1fa1f7e967302151554e42
A'zamjon <alijonovrobo@gmail.com>
2024-03-13 04:56:57 +0500

    Add project files.

---
+++ UserManagment.Console/Brokers/IStorageBroker.cs
@@ -0,0 +1,9 @@
+﻿using UserManagment.Console.Models;
+
+namespace UserManagment.Console.Brokers
+{
+    internal interface IStorageBroker
+    {
+        Credential[] GetAll();
+    }
+}
---
+++ UserManagment.Console/Brokers/StorageBroker.cs
@@ -0,0 +1,14 @@
+﻿using UserManagment.Console.Models;
+
+namespace UserManagment.Console.Brokers
+{
+    internal class StorageBroker : IStorageBroker
+    {
+        private readonly Credential[] credentials = 
+        {   
+            new Credential {Username = "A'zamjon", Password = "2303"}, 
+            new Credential {Username = "Yaqubaliy", Password = "1234"} 
+        };
+        public Credential[] GetAll() => credentials;
+    }
+}
